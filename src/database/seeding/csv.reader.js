2ee4312a0e7a4c50263a89ccc030c79de25bf314
Insidexa <nostresss77@gmail.com>
2018-11-21 11:25:38 +0200

    remove test data

--- src/database/seeding/csv.reader.js
+++ src/database/seeding/csv.reader.js
@@ -9,11 +9,7 @@ class CSVReader {
 
   parse(onParseRowCallback) {
     const data = fs.readFileSync(this.path).toString();
-    const lines = `
-    "1","A Dijiang","M",24,180,80,"China","CHN","1992 Summer",1992,"Summer","Barcelona","Basketball","Basketball Men's Basketball",NA
-"2","A Lamusi","M",23,170,60,"China","CHN","2012 Summer",2012,"Summer","London","Judo","Judo Men's Extra-Lightweight",NA
-
-    `.split('\n');
+    const lines = data.split('\n');
     lines.shift();
 
     lines.forEach(line => onParseRowCallback(line));
