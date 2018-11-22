111d80706287ee490351b3e78ffda0227d898b37
Insidexa <nostresss77@gmail.com>
2018-11-22 19:53:01 +0200

    add output

--- src/app/chart/cli.js
+++ src/app/chart/cli.js
@@ -1,5 +1,28 @@
 const { CLIParser } = require('./cli-parser');
+const { Output } = require('./output');
 
-const parser = new CLIParser(process.argv);
+class ChartCLI {
+  constructor(db) {
+    this.db = db;
+    this.output = new Output();
 
-parser.getCommand().handle();
+    this.initDBEvents();
+  }
+
+  run(argv) {
+    const parser = new CLIParser(argv);
+    const command = parser.getCommand(this.db);
+    const results = command.handle();
+
+    this.output.show(results, command.headerName);
+  }
+
+  initDBEvents() {
+    process.on('exit', () => this.db.close());
+    process.on('SIGINT', () => this.db.close());
+    process.on('SIGHUP', () => this.db.close());
+    process.on('SIGTERM', () => this.db.close());
+  }
+}
+
+exports.ChartCLI = ChartCLI;
