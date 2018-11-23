8cf72ce68f03d08e2b1b06216bc52482e5683420
Insidexa <nostresss77@gmail.com>
2018-11-23 13:05:09 +0200

    rewrite parsing to class, refactoring dependencies files

---
+++ src/app/csv/csv-parser.js
@@ -0,0 +1,119 @@
+const { CSVReader } = require('./csv.reader');
+const { AthletesCSV } = require('./mappers/athlete-csv.js');
+const { TeamCSV } = require('./mappers/team-csv.js');
+const { TYPE_SEASON } = require('../../models/game.js');
+const { GameCSV } = require('./mappers/game-csv.js');
+const { SportCSV } = require('./mappers/sport-csv.js');
+const { EventCSV } = require('./mappers/event-csv.js');
+const { uniqueBy } = require('../support/unique-by');
+
+class CSVParser {
+  constructor(path) {
+    this.parser = new CSVReader(path);
+    this.csv = [];
+    this.athletes = [];
+    this.teams = [];
+    this.games = [];
+    this.sports = [];
+    this.events = [];
+  }
+
+  parse() {
+    const unfilteredGames = [];
+    const allTeams = [];
+    const allSports = [];
+    const allEvents = [];
+
+    this.parser.parse((row, index) => {
+      const array = this.CSVStringToArray(row);
+
+      if (array) {
+        const unquotedArray = this.removeQuotesFromCSVStringArray(array);
+
+        const athleteCSV = new AthletesCSV(index, unquotedArray);
+        const teamCSV = new TeamCSV(unquotedArray, athleteCSV.getLastColumnNumber());
+        const gameCSV = new GameCSV(unquotedArray, teamCSV.getLastColumnNumber());
+        const sportCSV = new SportCSV(unquotedArray, gameCSV.getLastColumnNumber());
+        const eventCSV = new EventCSV(unquotedArray, sportCSV.getLastColumnNumber());
+        const isNotOfficial = !(gameCSV.model.year === 1906
+          && gameCSV.model.season === TYPE_SEASON.Summer);
+        if (isNotOfficial) {
+          this.athletes.push(athleteCSV);
+          unfilteredGames.push(gameCSV);
+          this.csv.push(unquotedArray);
+        }
+
+        allTeams.push(teamCSV);
+        allSports.push(sportCSV);
+        allEvents.push(eventCSV);
+      }
+    });
+
+    this.prepareData({
+      allTeams,
+      allSports,
+      allEvents,
+      unfilteredGames,
+    });
+  }
+
+  prepareData({
+    allTeams, allSports, allEvents, unfilteredGames,
+  }) {
+    this.teams = uniqueBy(allTeams, team => team.model.noc_name);
+    this.events = uniqueBy(allEvents, event => event.model.name);
+    this.sports = uniqueBy(allSports, sport => sport.model.name);
+    this.games = this.filterGamesWithMoreCities(unfilteredGames);
+  }
+
+  removeQuotesFromCSVStringArray(array) {
+    return array.map(item => this.removeQuotesIfNeeded(item));
+  }
+
+  removeQuotesIfNeeded(str) {
+    if (str[0] === '"') {
+      str = str.substr(1);
+    }
+
+    if (str[str.length - 1] === '"') {
+      str = str.substr(0, str.length - 1);
+    }
+
+    return str;
+  }
+
+  CSVStringToArray(row) {
+    return row.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g);
+  }
+
+  getGameCities(originalGame, unfilteredGames) {
+    const cities = [];
+
+    for (let i = 0; i < unfilteredGames.length; i++) {
+      const unfilteredGame = unfilteredGames[i].model;
+      const checkGame = originalGame.year === unfilteredGame.year
+        && originalGame.season === unfilteredGame.season;
+      if (checkGame) {
+        cities.push(unfilteredGame.city);
+      }
+    }
+
+    return cities;
+  }
+
+  filterGamesWithMoreCities(unfilteredGames) {
+    const games = [];
+    const uniquesGames = uniqueBy(unfilteredGames, game => `${game.model.season}${game.model.year}`);
+    uniquesGames.forEach((game) => {
+      const cities = this.getGameCities(game.model, unfilteredGames);
+      if (cities.length > 1) {
+        game.model.changeCitiesMultiple(cities, unfilteredGames);
+        games.push(game);
+      }
+    });
+
+    return games;
+  }
+}
+
+exports.CSVParser = CSVParser;
--- src/database/seeding/import.js
+++ src/database/seeding/import.js
@@ -1,7 +1,5 @@
 const sqlite3 = require('better-sqlite3');
-const {
-  athletes, teams, games, sports, events, csv,
-} = require('../../app/csv/parsing');
+const { CSVParser } = require('../../app/csv/csv-parser');
 const { TYPE_SEASON } = require('../../app/csv/mappers/game-csv.js');
 const { TYPE_MEDAL } = require('../../models/result.js');
 
@@ -67,17 +65,19 @@ function escapeQuotes(str) {
 }
 
 function prepareResult(db, row) {
-  const medal = TYPE_MEDAL[row[14]];
-  const eventId = db.prepare(`SELECT id FROM Events WHERE \`name\` = '${escapeQuotes(row[13])}'`).get().id;
-  const sportId = db.prepare('SELECT id FROM Sports WHERE `name` = ?').get(row[12]).id;
-  const city = escapeQuotes(row[11]);
+  const [seasonName, cityName, sportName, eventName, medalName] = row.slice(10, 15);
+  const medal = TYPE_MEDAL[medalName];
+  const eventId = db.prepare(`SELECT id FROM Events WHERE \`name\` = '${escapeQuotes(eventName)}'`).get().id;
+  const sportId = db.prepare('SELECT id FROM Sports WHERE `name` = ?').get(sportName).id;
+  const city = escapeQuotes(cityName);
 
-  const game = db.prepare(`SELECT id FROM Games WHERE year = ? AND season = ? AND city LIKE '%${city}%'`)
-    .get(row[9], TYPE_SEASON[row[10]]);
+  const gameId = db
+    .prepare(`SELECT id FROM Games WHERE year = ? AND season = ? AND city LIKE '%${city}%'`)
+    .get(row[9], TYPE_SEASON[seasonName]).id;
 
   return {
     medal,
-    game_id: game.id,
+    game_id: gameId,
     event_id: eventId,
     sport_id: sportId,
   };
@@ -98,7 +98,7 @@ VALUES (@athlete_id, @game_id, @sport_id, @event_id, @medal)
   insertMany(results);
 }
 
-function insertAthletes(db, athletes) {
+function insertAthletes(db, athletes, csv) {
   const preparedResults = [];
   const insert = db.prepare(`
 INSERT INTO Athletes(full_name, sex, year_of_birth, params, team_id)
@@ -129,11 +129,15 @@ VALUES (@full_name, @sex, @year_of_birth, @params, @team_id)
 
 function importFromCSV() {
   const db = sqlite3('./storage/olympic_history.db');
-  insertTeams(db, teams);
-  insertEvents(db, events);
-  insertSports(db, sports);
-  insertGames(db, games);
-  const results = insertAthletes(db, athletes);
+  const parser = new CSVParser('./storage/athlete_events.csv');
+
+  parser.parse();
+
+  insertTeams(db, parser.teams);
+  insertEvents(db, parser.events);
+  insertSports(db, parser.sports);
+  insertGames(db, parser.games);
+  const results = insertAthletes(db, parser.athletes, parser.csv);
   insertResults(db, results);
 
   process.on('exit', () => db.close());
