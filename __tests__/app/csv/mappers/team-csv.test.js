caec6b6abff67e57ddd7b1e94760f07ce6dc7a29
Insidexa <nostresss77@gmail.com>
2018-11-23 10:47:04 +0200

    add team csv mapper

--- __tests__/app/csv/mappers/team-csv.test.js
+++ __tests__/app/csv/mappers/team-csv.test.js
@@ -1,35 +1,15 @@
-const { AthletesCSV } = require('../../../../src/app/csv/mappers/athlete-csv');
-const { NA } = require('../../../../src/models/result');
+const { TeamCSV } = require('../../../../src/app/csv/mappers/team-csv');
 
-describe('AthletesCSV', () => {
-  const exampleAthleteCSVArray = [
-    1,
-    `Pedros (del'Torro)`,
-    'M',
-    '1930',
-    30,
-    'NA',
+describe('TeamCSV', () => {
+  const exampleTeamCSVArray = [
+    null,
+    'Team-2234',
+    'UKR',
   ];
 
-  const athlete = new AthletesCSV(0, exampleAthleteCSVArray, 0);
+  const team = new TeamCSV(exampleTeamCSVArray, 1);
 
-  it('should remove single quotes from string', () => {
-    expect(athlete.model.full_name).toBe('Pedros');
-  });
-
-  it('should parse year', () => {
-    expect(athlete.model.year_of_birth).toBe(1930);
-  });
-
-  it(`should return null if one of the params ${NA}`, () => {
-    expect(athlete.prepareParam(NA)).toBe(null);
-  });
-
-  it('should parsed height in params', () => {
-    expect(athlete.model.params.height).toBe(30);
-  });
-
-  it(`should parsed weight in params with csv-value ${NA}`, () => {
-    expect(athlete.model.params.weight).toBe(null);
+  it('should remove dash and numbers at end of the string', () => {
+    expect(team.model.name).toBe('Team');
   });
 });
