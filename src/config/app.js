71959672f1552b857735dc93add6f39233dfd16e
Insidexa <nostresss77@gmail.com>
2018-11-24 11:26:28 +0200

    use app config

---
+++ src/config/app.js
@@ -0,0 +1,6 @@
+const SQLITE3_DB = 'storage/olympic_history.db';
+
+const ATHLETE_EVENTS_CSV = 'storage/athlete_events.csv';
+
+exports.SQLITE3_DB = SQLITE3_DB;
+exports.ATHLETE_EVENTS_CSV = ATHLETE_EVENTS_CSV;
