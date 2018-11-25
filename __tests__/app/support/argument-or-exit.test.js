2ebb2223fda0402c4fbaade4c7e1ab70d7a0f1e6
Insidexa <nostresss77@gmail.com>
2018-11-25 11:28:36 +0200

    update test with check error

--- __tests__/app/support/argument-or-exit.test.js
+++ __tests__/app/support/argument-or-exit.test.js
@@ -1,21 +1,10 @@
-const { argumentOrExit } = require('../../../src/app/support/argument-or-exit.js');
+const { argumentOrError } = require('../../../src/app/support/argument-or-exit.js');
 
-describe('argumentOrExit', () => {
+describe('argumentOrError', () => {
   it('should exit with return code 1', () => {
-    // https://stackoverflow.com/questions/46148169/stubbing-process-exit-with-jest
-    const realProcess = process;
-    const exitMock = jest.fn();
-    // We assign all properties of the "real process" to
-    // our "mock" process, otherwise, if "myFunc" relied
-    // on any of such properties (i.e `process.env.NODE_ENV`)
-    // it would crash with an error like:
-    // `TypeError: Cannot read property 'NODE_ENV' of undefined`.
-    global.process = { ...realProcess, exit: exitMock };
-    argumentOrExit(undefined);
-    expect(exitMock).toHaveBeenCalledWith(1);
-    global.process = realProcess;
+    expect(argumentOrError).toThrow(Error);
   });
   it('argument with initialized value should return true', () => {
-    expect(argumentOrExit('winter')).toBeTruthy();
+    expect(argumentOrError('winter')).toBeTruthy();
   });
 });
